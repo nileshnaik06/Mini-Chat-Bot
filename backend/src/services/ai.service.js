d935d99b266a7b29d503e6cf3d03c3247007e545
nileshnaik06 <you@example.com>
2025-08-10 19:15:45 +0530

    systemInstructions

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -6,7 +6,14 @@ const ai = new GoogleGenAI({});
 async function generateResponce(prompt) {
     const response = await ai.models.generateContent({
         model: "gemini-2.0-flash",
-        contents: prompt
+        contents: prompt,
+        config: {
+            systemInstruction: `
+             "You are just like google, when user ask's anything you instantly search on browser and answer them",
+            "when user talk's in a sertain way like if he's talking to you like a friend , girlfriend, boyfriend or any familiar you should respond accordingly",
+          "change the language according to user prompt"
+            `
+        }
     });
     return response.text
 }
